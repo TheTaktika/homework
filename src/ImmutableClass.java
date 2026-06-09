efaee3601e888fbb81d04da64e1d46e5064a570a
Max <maxchumak24@gmail.com>
2026-06-09 20:16:43 +0300

    Module 1

---
+++ src/ImmutableClass.java
@@ -0,0 +1,38 @@
+public final class ImmutableClass {
+    private final NonImmutableClass nonImmutableClass;
+
+    public ImmutableClass (String name, int age) {
+        this.nonImmutableClass = new NonImmutableClass(name, age);
+    }
+
+    public ImmutableClass (NonImmutableClass nonImmutableClass) {
+        this.nonImmutableClass = new NonImmutableClass(nonImmutableClass.getName(), nonImmutableClass.getAge());
+    }
+
+    public NonImmutableClass getNonImmutableClass () {
+        return new  NonImmutableClass(
+                nonImmutableClass.getName(),
+                nonImmutableClass.getAge()
+        );
+    }
+
+    public String getName () {
+        return nonImmutableClass.getName();
+    }
+
+    public int getAge () {
+        return nonImmutableClass.getAge();
+    }
+
+    static void main(String[] args) {
+        NonImmutableClass mutable = new NonImmutableClass("Vlad", 25);
+        ImmutableClass immutable = new ImmutableClass(mutable);
+
+        System.out.println(immutable.getAge());
+        System.out.println(mutable.getAge());
+
+        mutable.setAge(50);
+        System.out.println(immutable.getAge());
+        System.out.println(mutable.getAge());
+    }
+}
---
+++ src/NonImmutableClass.java
@@ -0,0 +1,29 @@
+public class NonImmutableClass {
+
+    private String name;
+    private int age;
+
+    public NonImmutableClass (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public NonImmutableClass(NonImmutableClass mutable) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
