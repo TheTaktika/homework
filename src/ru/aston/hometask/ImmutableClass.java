2458fb94d9d908d8d02f292c3fd1d04fbdc2ae39
Max <maxchumak24@gmail.com>
2026-06-16 14:34:02 +0300

    correction of mistakes

--- src/ru/aston/hometask/ImmutableClass.java
+++ src/ru/aston/hometask/ImmutableClass.java
@@ -1,32 +1,32 @@
-package Module1;
+package ru.aston.hometask;
 
 public final class ImmutableClass {
     private final NonImmutableClass nonImmutableClass;
 
-    public ImmutableClass (String name, int age) {
+    public ImmutableClass(String name, int age) {
         this.nonImmutableClass = new NonImmutableClass(name, age);
     }
 
-    public ImmutableClass (NonImmutableClass nonImmutableClass) {
+    public ImmutableClass(NonImmutableClass nonImmutableClass) {
         this.nonImmutableClass = new NonImmutableClass(nonImmutableClass.getName(), nonImmutableClass.getAge());
     }
 
-    public NonImmutableClass getNonImmutableClass () {
-        return new  NonImmutableClass(
+    public NonImmutableClass getNonImmutableClass() {
+        return new NonImmutableClass(
                 nonImmutableClass.getName(),
                 nonImmutableClass.getAge()
         );
     }
 
-    public String getName () {
+    public String getName() {
         return nonImmutableClass.getName();
     }
 
-    public int getAge () {
+    public int getAge() {
         return nonImmutableClass.getAge();
     }
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         NonImmutableClass mutable = new NonImmutableClass("Vlad", 25);
         ImmutableClass immutable = new ImmutableClass(mutable);
 
